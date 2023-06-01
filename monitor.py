de887b334b8282c573bfc79c0bc46abc4294ba39
N-Erickson <nickolas.b.erickson@gmail.com>
2023-05-31 21:53:49 -0500

    Update monitor.py
    Uptime checking occurs regardless of browser being active now

--- monitor.py
+++ monitor.py
@@ -6,6 +6,8 @@ from jinja2 import select_autoescape, Environment, FileSystemLoader
 from collections import deque
 from datetime import datetime, timedelta
 import sqlite3
+import threading
+import time
 
 app = Flask(__name__)
 
@@ -21,6 +23,8 @@ if not os.path.exists(database_path):
     conn.close()
 
 ping_history = {}
+ping_interval = 5  # Interval in seconds for pinging the endpoints
+
 
 def calculate_uptime(pings):
     if not pings:
@@ -30,6 +34,7 @@ def calculate_uptime(pings):
     uptime_percentage = (successful_pings / len(pings)) * 100
     return round(uptime_percentage, 2)
 
+
 def calculate_response_time(pings):
     if not pings:
         return None
@@ -38,6 +43,7 @@ def calculate_response_time(pings):
     average_response_time = total_response_time / len(pings)
     return round(average_response_time, 2)
 
+
 env = Environment(
     loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     autoescape=select_autoescape(['html', 'xml'])
@@ -45,38 +51,53 @@ env = Environment(
 env.filters['calculate_uptime'] = calculate_uptime
 env.filters['calculate_response_time'] = calculate_response_time
 
+
+def perform_ping(endpoint):
+    try:
+        start_time = datetime.now()
+        response = requests.get(endpoint)
+        response_time = datetime.now() - start_time
+        status = response.ok
+    except requests.exceptions.RequestException:
+        response_time = None
+        status = False
+
+    if endpoint not in ping_history:
+        ping_history[endpoint] = deque(maxlen=10)
+
+    ping_history[endpoint].append((status, response_time))
+
+    # Store the ping data in the database
+    conn = sqlite3.connect(database_path)
+    c = conn.cursor()
+    c.execute("INSERT INTO pings VALUES (?, ?, ?, ?)",
+              (endpoint, int(status), response_time.total_seconds() if response_time else None, str(datetime.now())))
+    conn.commit()
+    conn.close()
+
+
+def ping_endpoints():
+    while True:
+        with open('config.yaml') as config_file:
+            config = yaml.safe_load(config_file)
+
+        for endpoint in config['endpoints']:
+            perform_ping(endpoint)
+
+        # Sleep for the specified interval before pinging again
+        time.sleep(ping_interval)
+
+
 @app.route('/')
 def home():
-    with open('config.yaml') as config_file:
-        config = yaml.safe_load(config_file)
-
-    for endpoint in config['endpoints']:
-        if endpoint not in ping_history:
-            ping_history[endpoint] = deque(maxlen=10)
-
-        try:
-            start_time = datetime.now()
-            response = requests.get(endpoint)
-            response_time = datetime.now() - start_time
-            status = response.ok
-        except requests.exceptions.RequestException:
-            response_time = None
-            status = False
-
-        ping_history[endpoint].append((status, response_time))
-
-        # Store the ping data in the database
-        conn = sqlite3.connect(database_path)
-        c = conn.cursor()
-        c.execute("INSERT INTO pings VALUES (?, ?, ?, ?)",
-                  (endpoint, int(status), response_time.total_seconds() if response_time else None, str(datetime.now())))
-        conn.commit()
-        conn.close()
-
     template = env.get_template('index.html')
     rendered_template = template.render(results=ping_history)
 
     return render_template_string(rendered_template)
 
+
 if __name__ == '__main__':
+    # Start the background thread for pinging endpoints
+    threading.Thread(target=ping_endpoints, daemon=True).start()
+
     app.run(debug=True)
